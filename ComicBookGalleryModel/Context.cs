95365579669c5b9e73f44858f5c66c7e949fa22d
Spencer Bell <spencerb3@live.ca>
2019-05-02 17:40:24 -0600

    Initial ComicBookGalleryModel Setup and test

---
+++ ComicBookGalleryModel/Context.cs
@@ -0,0 +1,18 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        public DbSet<ComicBook> ComicBooks { get; set; }
+
+
+
+    } //END Class
+} //END namespace
---
+++ ComicBookGalleryModel/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                context.ComicBooks.Add(new Models.ComicBook()
+                {
+                    SeriesTitle = "The Amazing Spider-Man",
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today
+                });
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks.ToList(); //Will retrive all comic books from the database(No order)
+                foreach(var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.SeriesTitle);
+                }
+
+                Console.ReadLine();
+
+            }
+
+            
+
+
+
+
+        }
+    } //END Class
+} //END namespace
